5603244b2d6ff2c1dfce622d0be0a27bb41b02bd
Debjit Pal <debjitpal5040@gmail.com>
2022-11-23 16:02:13 +0530

    polishing

--- imageprocess/views.py
+++ imageprocess/views.py
@@ -1,31 +1,33 @@
-from fileinput import filename
-from django.shortcuts import render
-from django.http import HttpResponse
-from subprocess import run,PIPE
-from django.core.files.storage import FileSystemStorage
+"""views.py"""
 import sys
+from subprocess import run, PIPE
+from django.shortcuts import render
+from django.core.files.storage import FileSystemStorage
+
 
 def home(request):
-    return render(request,'home.html')
+    """Home page"""
+    return render(request, 'home.html')
+
 
 def filter_selection(request):
-    img=request.FILES['image']
-    fs=FileSystemStorage()
-    fs.delete('temp.jpeg')
-    filename=fs.save('temp.jpeg',img)
-    # fullurl=fs.open(filename)
-    fileurl=fs.url(filename)
-    return render(request,'filter_selection.html',{'raw_image':fileurl})
+    """Filter selection"""
+    img = request.FILES['image']
+    _fs = FileSystemStorage()
+    _fs.delete('temp.jpeg')
+    filename = _fs.save('temp.jpeg', img)
+    fileurl = _fs.url(filename)
+    return render(request, 'filter_selection.html', {'raw_image': fileurl})
+
 
 def result(request):
-    filter=request.POST.get('filter')
-    # img=request.FILES['image']
-    fs=FileSystemStorage()
-    # fs.delete('temp.jpeg')
-    # filename=fs.save('temp.jpeg',img)
-    filename='temp.jpeg'
-    fullurl=fs.open(filename)
-    fileurl=fs.url(filename)
-    res=run([sys.executable,'templates//filter.py',str(fullurl),str(filename),filter],shell=False,stdout=PIPE)
-    resurl=fs.url(str(res.stdout).split('\'')[1])
-    return render(request,'result.html',{'raw_image':fileurl,'processed_image':resurl})
+    """Result page"""
+    _filter = request.POST.get('filter')
+    _fs = FileSystemStorage()
+    filename = 'temp.jpeg'
+    fullurl = _fs.open(filename)
+    fileurl = _fs.url(filename)
+    res = run([sys.executable, 'templates//filter.py', str(fullurl),
+            str(filename), _filter], shell=False, stdout=PIPE, check=True)
+    resurl = _fs.url(str(res.stdout).split('\'')[1])
+    return render(request, 'result.html', {'raw_image': fileurl, 'processed_image': resurl})
